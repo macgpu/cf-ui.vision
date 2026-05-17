4fec2c7b5dab4cbc450e53018954b64d46ae8432
Ui.Vision <teamdocs99+github@gmail.com>
2026-05-17 10:42:26 +0200

    remove

---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OCRReaderMVCWeb.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult NotFound()
+        {
+          //  string ipAddress = Request.ServerVariables["REMOTE_ADDR"] + " , ";
+        //    Logger.Error(ipAddress + "Some error occurred. Error page requested");
+            return View();
+        }
+    }
+}
